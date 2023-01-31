4b4d4c7db204840888d50d110b768f6e31efc7e0
nevsk <nevskyniki@gmail.com>
2023-01-31 17:57:11 +0300

    commit, eng keyboard stop

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
-from random import *
+print(ord('А'))
+print(ord('Я'))
+print(ord('а'))
+print(ord('я'))
+print(ord(','))
+print(ord('a'))
+print(ord('z'))
+print(ord('A'))
+print(ord('Z'))
+const_upper_en = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const_lower_en = 'abcdefghijklmnopqrstuvwxyz'
+
+def encryption_text(text, shift):
+    result = ''
+    for i in text:
+        ord_i = ord(i)
+        if i in ',./!?;: ':
+            encry = ord_i
+        else:
+            encry = ord_i + shift
+        if i.isupper() and encry >= 122 or (i.islower() and encry <= 200):
+            encry -= 26
+        elif i.isupper() and encry > 1071 or (i.islower() and encry > 1103):
+            encry -= 32
+        result += chr(encry)
+    return result
 
 
-digits = '0123456789'
-lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-punctuation = '!#$%&*+-=?@^_'
-count_pwd = int(input('Введите количество создаваемых паролей: '))
-count = 0
-chars = ''
-
-dig_on = input('Включать ли цифры 0123456789? (y/n) ').lower()
-if dig_on in ['да', 'д', 'l', 'lf', 'y', 'yes']:
-    chars += digits
-abc_upper = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (y/n) ')
-if abc_upper in ['да', 'д', 'l', 'lf', 'y', 'yes']:
-    chars += lowercase_letters
-abc_lower = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (y/n) ')
-if abc_lower in ['да', 'д', 'l', 'lf', 'y', 'yes']:
-    chars += uppercase_letters
-ch_on = input('Включать ли символы !#$%&*+-=?@^_? (y/n) ')
-if ch_on in ['да', 'д', 'l', 'lf', 'y', 'yes']:
-    chars += punctuation
-exc_on = input('Исключать ли неоднозначные символы il1Lo0O? (y/n) ')
-if exc_on in ['да', 'д', 'l', 'lf', 'y', 'yes']:
-    for i in ('il1Lo0O'):
-        if i in chars:
-            chars = chars.replace(i, '')
-
-def generate_pwd():
-    pwd = ''
-    len_pwd = int(input('Укажите длину пароля: '))
-    for i in range(len_pwd):
-        pwd += choice(chars)
-    return pwd
+def input_text():
+    text = input('Введите ваш текст: ')
+    shift = int(input('Введите сдвиг вправо: '))
+    print(encryption_text(text, shift))
 
 
-def create_pwd_count():
-    for i in range(count_pwd):
-        print(generate_pwd())
-
-
-create_pwd_count()
+input_text()
